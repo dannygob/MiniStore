c6f67159017b227c6d5ba13f15c70269880e178f
Tu Nombre <computadoraesp@gmail.com>
2025-05-31 10:01:27 +0200

    Fix: Correct applicationId in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.minstore"
+        applicationId = "com.example.ministore"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
